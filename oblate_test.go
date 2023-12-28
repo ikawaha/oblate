70d92e5e053a3af9fe7523012822165b3bc2948c
ikawaha <ikawaha@users.noreply.github.com>
2023-12-28 22:46:02 +0900

    doc: Update

--- oblate_test.go
+++ oblate_test.go
@@ -12,15 +12,23 @@ import (
 func Example() {
 	e1 := errors.New("e1")
 	e2 := errors.New("e2")
-	e3 := io.EOF
+	e3 := fmt.Errorf("wrapped error: %w", io.EOF)
+
+	// New returns an error that wraps the given errors.
 	err := oblate.New("user-facing error message", e1, e2, e3)
 
+	// The error formats as the strings obtained by calling the Error
+	// method of the first error in errs.
 	fmt.Println("error:")
 	fmt.Println(err.Error())
 
+	// Cause returns the error formats as the concatenation of the strings
+	// obtained by calling the Error method of each element of errs
+	// except the first error, with a newline between each string.
 	fmt.Println("cause:")
 	fmt.Println(err.(*oblate.Error).Cause())
 
+	// The error wrapped by oblate can be checked with errors.Is and errors.As.
 	fmt.Println("error details:")
 	fmt.Printf("errors.Is(err, io.EOF): %v\n", errors.Is(err, io.EOF))
 
